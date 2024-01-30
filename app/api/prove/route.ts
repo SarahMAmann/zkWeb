725053351740c0b9aa693349d5fde0e998c13f79
Sarah Benson <sarah@Sarahs-MacBook-Pro.local>
2024-01-29 20:44:29 -0600

    error logging

--- app/api/prove/route.ts
+++ app/api/prove/route.ts
@@ -44,6 +44,6 @@ export async function POST(request: Request) {
 
     return NextResponse.json({ isCorrect, error });
   } catch (error) {
-    return NextResponse.json({ error: "An error occurred" }, { status: 500 });
+    return NextResponse.json({ error: error });
   }
 }
